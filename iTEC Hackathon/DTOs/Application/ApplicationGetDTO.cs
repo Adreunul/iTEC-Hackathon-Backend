0a205fbde7e85ba9c2f50b9809d57472e740a29a
Adrian Danila <adrian-danila@365.univ-ovidius.ro>
2024-04-06 00:10:27 +0300

    XAXA7

--- iTEC Hackathon/DTOs/Application/ApplicationGetDTO.cs
+++ iTEC Hackathon/DTOs/Application/ApplicationGetDTO.cs
@@ -5,7 +5,7 @@
         public int IdApplication { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public int IdUserAuthor { get; set; }
+        public string UserEmail { get; set; }
         public string ApplicationState { get; set; }
 
     }
