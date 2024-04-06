2f808f6eb8b98674bd1efcf4b9f8f9f4441826e6
Adrian Danila <adrian-danila@365.univ-ovidius.ro>
2024-04-06 23:20:57 +0300

    xaxa20

--- iTEC Hackathon/Repositories/User/RegisterUserRepository.cs
+++ iTEC Hackathon/Repositories/User/RegisterUserRepository.cs
@@ -3,6 +3,8 @@ using iTEC_Hackathon.DTOs.User;
 using iTEC_Hackathon.Interfaces;
 using iTEC_Hackathon.Interfaces.User;
 using System.Data;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace iTEC_Hackathon.Repositories
 {
@@ -18,12 +20,29 @@ namespace iTEC_Hackathon.Repositories
         {
             var parameters = new DynamicParameters();
             parameters.Add("@Email", userCredentialsDTO.Email);
-            parameters.Add("@Password", userCredentialsDTO.Password);
+
+            string hashedPassword = HashPassword(userCredentialsDTO.Password);
+            parameters.Add("@Password", hashedPassword);
+
             parameters.Add("@UserID", dbType: DbType.Int32, direction: ParameterDirection.Output);
             using (var connection = _dbConnectionFactory.ConnectToDataBase())
             {
                 await connection.ExecuteAsync("RegisterUser", parameters, commandType: CommandType.StoredProcedure);
                 return parameters.Get<int>("UserID");
+            }
+        }
+
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
             }
         }
     }
